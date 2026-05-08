541d3ffef8a1bb560a5d33a32276ff1479e95b0e
ZhangJiaHui2005 <huygiatruong2005@gmail.com>
2026-05-08 11:06:58 +0700

    add laravel react inertia project

---
+++ my_app/resources/js/Pages/Hello.jsx
@@ -0,0 +1,7 @@
+export default function Welcome({ user }) {
+    return (
+        <div className="p-6">
+            <h1 className="text-2xl font-bold">Chào {user}, bạn đã cài thành công!</h1>
+        </div>
+    );
+}
