9e14ff7edc4f52c0c391d259d654c986bf218166
ZhangJiaHui2005 <huygiatruong2005@gmail.com>
2026-05-09 13:28:41 +0700

    remove old project

--- my_app/resources/js/Pages/Hello.jsx
+++
@@ -1,7 +0,0 @@
-export default function Welcome({ user }) {
-    return (
-        <div className="p-6">
-            <h1 className="text-2xl font-bold">Chào {user}, bạn đã cài thành công!</h1>
-        </div>
-    );
-}
