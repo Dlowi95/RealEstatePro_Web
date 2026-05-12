ecf75e5ebf32c313dfd162dd1e1d5d35d0a2fd2d
Duybua12358 <phamduyanhkg@gmail.com>
2026-05-12 22:23:08 +0700

    update backend ket noi user thanh cong

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,9 +4,9 @@ const connectDB = async () => {
 
   try {
 
-    await mongoose.connect(
-      process.env.MONGO_URI
-    );
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "RealEstatePro",
+    });
 
     console.log("MongoDB Connected");
 
